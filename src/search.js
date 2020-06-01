0007891e0e88decf66638909554a85ee620eb6ce
presenty <presenty1897@gmail.com>
2020-06-01 17:56:49 +0900

    made upload file preivew

--- src/search.js
+++ src/search.js
@@ -1,13 +1,27 @@
 var data = null;
-window.onload = function () {
+window.onload = function() {
+    let fileDataController = {};
+    let tempFileData = {};
+    (function setupInputFileChange() {
+        $('#inputFile').on('change', (e) => {
+            var fileName = $(e.target).val();
+            $(e.target).next('.custom-file-label').html(fileName);
+
+            tempFileData = {};
+            ([...$('#inputFile')[0].files]).forEach(file => reader.readAsText(file, "euc-kr"));
+
+        });
+    })();
+
     let dropArea = document.getElementById('dragdropdiv');
     let callAjaxButton = document.getElementById('api_start_button');
     let downloadButton = document.getElementById('download_result_button');
     let reader = new this.FileReader();
     var count = 0;
-    reader.onload = function () {
+    reader.onload = function() {
+        tempFileData.rawData = reader.result;
         var rows = reader.result.split('\r\n');
-        data = rows.filter(row => row != "").map(row => {
+        data = rows.slice(0, 20).filter(row => row != "").map(row => {
             var exceptCol = row.split('"');
             var col = exceptCol
                 .filter(element => element != "")
@@ -20,13 +34,68 @@ window.onload = function () {
         });
         data.forEach(cols => cols.forEach(element => element.split('"').join('').split('\r\n').join('')));
         dropArea.textContent = '0/' + data.length;
+        setDataPreviewTable(data);
     };
 
-    callAjaxButton.onclick = function () {
+    var initializeTable = function() {
+        var table = $('#selected_file_table');
+        table.empty();
+    }
+    var setDataPreviewTable = function(data) {
+        initializeTable();
+        var table = $('#selected_file_table');
+        let column_count;
+
+        function makeThAndAppend(text, scope) {
+            var th = document.createElement('th');
+            th.setAttribute('scope', scope);
+            th.innerText = text;
+            return th;
+        };
+
+        (function makeTableHeader() {
+            var head = document.createElement('thead');
+            table.append(head);
+            var tr = document.createElement('tr');
+            head.append(tr);
+            column_count = data.reduce((count, row) => {
+                if (row.length > count) count = row.length;
+                return count;
+            }, 0);
+
+            tr.append(makeThAndAppend('#', 'col'));
+            Array.from({
+                length: column_count
+            }, (_, index) => {
+                tr.append(makeThAndAppend('#' + index, 'col'));
+            });
+        })();
+
+        (function fillTable() {
+            var body = document.createElement('tbody');
+            table.append(body);
+
+            data.forEach((row, index) => {
+                var tr = document.createElement('tr');
+                body.append(tr);
+                tr.append(makeThAndAppend(index, 'row'));
+
+                Array.from({
+                    length: column_count
+                }, (_, index) => {
+                    var td = document.createElement('td');
+                    td.innerText = row[index];
+                    tr.append(td);
+                });
+            })
+        })();
+    };
+
+    callAjaxButton.onclick = function() {
         if (data != null) {
             data[0].push('result');
             data.slice(1, data.length).forEach(address => {
-                callJusoAPI(address[1], function (e) {
+                callJusoAPI(address[1], function(e) {
                     count++;
                     dropArea.textContent = count + '/' + data.length;
                     if (typeof e.results.juso[0] != 'undefined')
@@ -35,11 +104,11 @@ window.onload = function () {
             });
         };
     };
-    downloadButton.onclick = function () {
+    downloadButton.onclick = function() {
         makeResultDownloadButton();
     };
 
-    var makeResultDownloadButton = function () {
+    var makeResultDownloadButton = function() {
         var encodedUri = (function makeResult() {
             let csvContent = new Blob([
                 new Uint8Array([0xEF, 0xBB, 0xBF]),
@@ -102,7 +171,7 @@ window.onload = function () {
                 keyword: keyword
             },
             success: successCallback,
-            error: function (e) {
+            error: function(e) {
                 let errorDiv = document.getElementById('errordiv');
                 let p = document.createElement('p');
                 p.innerText = e;
