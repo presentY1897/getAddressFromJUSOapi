ff11f42104746a75add5d70d1097db6fee76223b
presenty <presenty1897@gmail.com>
2020-06-02 11:29:19 +0900

    pop out file load func

--- src/search.js
+++ src/search.js
@@ -1,95 +1,11 @@
 var data = null;
 window.onload = function() {
     let fileDataController = {};
-    let tempFileData = {};
-    (function setupInputFileChange() {
-        $('#inputFile').on('change', (e) => {
-            var fileName = $(e.target).val();
-            $(e.target).next('.custom-file-label').html(fileName);
-
-            tempFileData = {};
-            ([...$('#inputFile')[0].files]).forEach(file => reader.readAsText(file, "euc-kr"));
-
-        });
-    })();
 
     let dropArea = document.getElementById('dragdropdiv');
     let callAjaxButton = document.getElementById('api_start_button');
     let downloadButton = document.getElementById('download_result_button');
-    let reader = new this.FileReader();
     var count = 0;
-    reader.onload = function() {
-        tempFileData.rawData = reader.result;
-        var rows = reader.result.split('\r\n');
-        data = rows.slice(0, 20).filter(row => row != "").map(row => {
-            var exceptCol = row.split('"');
-            var col = exceptCol
-                .filter(element => element != "")
-                .map((element, index) => {
-                    if (index % 2 == 0) return element.split(',').filter(e => e != "")
-                    else return element
-                })
-                .reduce((acc, curr) => acc.concat(curr));
-            return col;
-        });
-        data.forEach(cols => cols.forEach(element => element.split('"').join('').split('\r\n').join('')));
-        dropArea.textContent = '0/' + data.length;
-        setDataPreviewTable(data);
-    };
-
-    var initializeTable = function() {
-        var table = $('#selected_file_table');
-        table.empty();
-    }
-    var setDataPreviewTable = function(data) {
-        initializeTable();
-        var table = $('#selected_file_table');
-        let column_count;
-
-        function makeThAndAppend(text, scope) {
-            var th = document.createElement('th');
-            th.setAttribute('scope', scope);
-            th.innerText = text;
-            return th;
-        };
-
-        (function makeTableHeader() {
-            var head = document.createElement('thead');
-            table.append(head);
-            var tr = document.createElement('tr');
-            head.append(tr);
-            column_count = data.reduce((count, row) => {
-                if (row.length > count) count = row.length;
-                return count;
-            }, 0);
-
-            tr.append(makeThAndAppend('#', 'col'));
-            Array.from({
-                length: column_count
-            }, (_, index) => {
-                tr.append(makeThAndAppend('#' + index, 'col'));
-            });
-        })();
-
-        (function fillTable() {
-            var body = document.createElement('tbody');
-            table.append(body);
-
-            data.forEach((row, index) => {
-                var tr = document.createElement('tr');
-                body.append(tr);
-                tr.append(makeThAndAppend(index, 'row'));
-
-                Array.from({
-                    length: column_count
-                }, (_, index) => {
-                    var td = document.createElement('td');
-                    td.innerText = row[index];
-                    tr.append(td);
-                });
-            })
-        })();
-    };
 
     callAjaxButton.onclick = function() {
         if (data != null) {
