081e38f03de06e77c3a521a44478f78fb65ea15b
presenty <presenty1897@gmail.com>
2020-06-15 17:20:25 +0900

    call ajax as seperately

--- src/conversion.js
+++ src/conversion.js
@@ -16,19 +16,19 @@
                 type: 'doughnut',
                 data: {
                     datasets: [{
-                        data: [data.length, 0, 0],
+                        data: [data.length, 0, 0, 0],
                         backgroundColor: [
                             'rgba(89, 44, 28, 1)',
                             'rgba(205, 217, 199, 1)',
-                            'rgba(92, 115, 57, 1)'
+                            'rgba(92, 115, 57, 1)',
+                            'rgba(238, 28, 28, 1)'
                         ]
                     }],
-
-                    // These labels appear in the legend and in the tooltips when hovering different arcs
                     labels: [
                         '남은 데이터',
                         '분석 중',
-                        '변환 완료'
+                        '변환 완료',
+                        '변환 실패'
                     ]
                 },
                 options: {}
@@ -41,7 +41,11 @@
             }
         })();
 
-        var count = 0;
+        var convsersionCount = 0;
+        var stackCount = 0;
+        var failCount = 0;
+
+        let stepSize = 500;
         (function callApiByAjax() {
             data[0].push('result');
 
@@ -50,44 +54,70 @@
                 data[0].push('positionY');
             }
 
-            data.slice(1, data.length).forEach(address => {
-                callJusoAPI(
-                    'http://www.juso.go.kr/addrlink/addrLinkApiJsonp.do', {
-                        currentpage: 1,
-                        countPerPage: 1,
-                        resultType: 'json',
-                        confmKey: addressApiKey,
-                        keyword: address[1]
-                    },
-                    e => {
-                        count++;
-                        chartDataUpdate([data.length - count, 0, count]);
-                        console.log(count);
-                        if (typeof e.results.juso[0] != 'undefined') {
-                            if (conversionSelect == '도로명찾기') address.push(e.results.juso[0].roadAddr);
-                            else if (conversionSelect == '주소찾기') address.push(e.results.juso[0].jibunAddr);
-                        };
-                        if (isCallPositionApi) {
-                            var result = e.results.juso[0];
-                            if (result != undefined)
-                                callJusoAPI(
-                                    'http://www.juso.go.kr/addrlink/addrCoordApiJsonp.do', {
-                                        admCd: result.admCd,
-                                        rnMgtSn: result.rnMgtSn,
-                                        udrtYn: result.udrtYn,
-                                        buldMnnm: result.buldMnnm,
-                                        buldSlno: result.buldSlno,
-                                        confmKey: positionApiKey,
-                                        resultType: 'json'
-                                    },
-                                    e => {
-                                        address.push(e.results.juso[0].entX);
-                                        address.push(e.results.juso[0].entY);
-                                    }
-                                )
-                        }
+            let chartUpdater = setInterval(() => {
+                chartDataUpdate([data.length - stackCount, stackCount - convsersionCount, convsersionCount - failCount, failCount]);
+            }, 150);
+            const callAddressPromise = function(rows) {
+                return new Promise((resolve) => {
+                    rows.forEach(address => {
+                        callJusoAPI(
+                            'http://www.juso.go.kr/addrlink/addrLinkApiJsonp.do', {
+                                currentpage: 1,
+                                countPerPage: 1,
+                                resultType: 'json',
+                                confmKey: addressApiKey,
+                                keyword: address[1]
+                            },
+                            e => {
+                                convsersionCount++;
+                                if (typeof e.results.juso[0] != 'undefined') {
+                                    if (conversionSelect == '도로명찾기') address.push(e.results.juso[0].roadAddr);
+                                    else if (conversionSelect == '주소찾기') address.push(e.results.juso[0].jibunAddr);
+                                } else { failCount++; };
+                                if (isCallPositionApi) {
+                                    var result = e.results.juso[0];
+                                    if (result != undefined)
+                                        callJusoAPI(
+                                            'http://www.juso.go.kr/addrlink/addrCoordApiJsonp.do', {
+                                                admCd: result.admCd,
+                                                rnMgtSn: result.rnMgtSn,
+                                                udrtYn: result.udrtYn,
+                                                buldMnnm: result.buldMnnm,
+                                                buldSlno: result.buldSlno,
+                                                confmKey: positionApiKey,
+                                                resultType: 'json'
+                                            },
+                                            e => {
+                                                address.push(e.results.juso[0].entX);
+                                                address.push(e.results.juso[0].entY);
+                                                if (convsersionCount == stackCount) resolve(convsersionCount);
+                                            }
+                                        )
+                                } else {
+                                    if (convsersionCount == stackCount) resolve(convsersionCount);
+                                }
+                            });
                     });
-            });
+                })
+            }
+            data.slice(1, data.length).reduce((steps, step) => {
+                if (steps[steps.length - 1] == null) {
+                    var newArr = [step];
+                    steps.push(newArr);
+                } else if (steps[steps.length - 1].length < stepSize) {
+                    steps[steps.length - 1].push(step);
+                } else {
+                    var newArr = [step];
+                    steps.push(newArr);
+                }
+                return steps;
+            }, []).reduce((promise, rows) => {
+                return promise.then(() => {
+                    count = 0;
+                    stackCount += rows.length;
+                    return callAddressPromise(rows);
+                });
+            }, Promise.resolve());
         })();
     });
 
