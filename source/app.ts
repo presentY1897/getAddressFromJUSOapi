1428b10382f47a550e5177988ec1eb6020b99968
presenty1897 <presenty1897@gmail.com>
2020-08-30 10:28:19 +0900

    update a file input event

--- source/app.ts
+++ source/app.ts
@@ -9,14 +9,15 @@ let fileEncoding = 'euc-kr';
 let fileInputId = 'inputFile';
 let fileInputElement: HTMLInputElement = document.getElementById(fileInputId) as HTMLInputElement;
 
-let fileInputReact: (file: File) => void = function (file) {
+let fileInputReact: (value: File, index: number, array: File[]) => void = function (file) {
     let inputFile = new csvFile({ file: file, encoding: fileEncoding });
     inputFileController.addFile(inputFile);
     inputFileController.targetFile = inputFile;
+    fileInputElement.innerText = file.name;
 };
 let fileInputElementEvent: (this: HTMLInputElement, ev: Event) => void = function (_) {
     (function removeFileControllerTarget() { inputFileController.targetFile = null })();
-    (function insertFileWhenInputFileIsNotNull() { fileInputElement.files !== null ? [...fileInputElement.files].forEach(fileInputReact) : null });
+    (function insertFileWhenInputFileIsNotNull() { fileInputElement.files !== null ? [...fileInputElement.files].forEach(fileInputReact) : null })();
 };
 
 (function initFileInputEvent() {
@@ -25,9 +26,11 @@ let fileInputElementEvent: (this: HTMLInputElement, ev: Event) => void = functio
 
 
 let pageViewControl = new pageViewController(
-    [new tabPage('dashboardTab', 'dashboard_page'),
-    new tabPage('fileUploadTab', 'fileupload_page'),
-    new tabPage('workTab', 'work_page')],
+    [
+        new tabPage('dashboardTab', 'dashboard_page'),
+        new tabPage('fileUploadTab', 'fileupload_page'),
+        new tabPage('workTab', 'work_page')
+    ],
     (target: { classList: { add: (arg0: string) => any; }; }) => target.classList.add('d-none'),
     (target: { classList: { remove: (arg0: string) => any; }; }) => target.classList.remove('d-none')
 )
